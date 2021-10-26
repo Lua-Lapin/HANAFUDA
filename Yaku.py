7599fabb144068244935800d94d16ab5b35935b5
Lua_lapin <40061241+Lua-Lapin@users.noreply.github.com>
2021-10-26 15:57:49 +0900

    update yaku.py

--- Yaku.py
+++ Yaku.py
@@ -15,3 +15,10 @@ class Yaku():
   def point(self,cards):
     for i in range(self.yakuCount):
       self.check(cards,i)
+
+# 5光 11は雨 1a,3a,8a,11a,12a
+# 10a酒 3a桜 8a月
+# 7a猪 10a鹿 6a蝶
+# 赤短1 2 3 b
+# 青短6 9 10b
+# 短4 5 7b
