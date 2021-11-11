8383737231f148928c5db4fbefd1d3e969622e18
Lua_lapin <40061241+Lua-Lapin@users.noreply.github.com>
2021-11-11 14:31:57 +0900

    updete

--- Enemy.py
+++ Enemy.py
@@ -1,13 +1,27 @@
 from Player import Player
+from Yaku import Yaku
+from Field import Field 
 import random
 
 class Enemy(Player):
+  priority = [] # 役の優先順位作って
+  yaku = Yaku()
   def __init__(self):
     super().__init__()
-    priority = [] # 役の優先順位作って
 
-  def search(self):
-    pass
+  def search(self,cards):
+    point = 0
+    # point += self.countPoint(self.yaku.countYaku(self.getTicket()))
+    for i in self.getHand():
+      dupl = self.getTicket().copy()
+      dupl.append(self.judgeCard(i,cards))
+      out = self.yaku.check(self.getTicket())
+      
+
+
+
+  def judgeCard(self,card,ba):
+    return [s for s in ba if card[0] in s]
 
   # ランダムに選んでカードを返す
   def random(self):
